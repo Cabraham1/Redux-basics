29817e5a5dd9d1cf0949ea93ded52ba9a06fe57f
cabraham1 <christopherabraham8@gmail.com>
2023-04-09 19:50:08 +0100

    working with dispatch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/CartContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import CartItem from "./CartItem";
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+
+const CartContainer = () => {
+  const dispatch = useDispatch();
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>You bag</h2>
+          <h4 className="empty-cart">is currently Empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className="cart">
+      <header>
+        <h2>Your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            Total <span>{total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          Claer All
+        </button>
+      </footer>
+    </section>
+  );
+};
+
+export default CartContainer;
